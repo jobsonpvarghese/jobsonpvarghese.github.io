ffd068482972927c871e6a1cf204caf8a7567f57
Jobson P Varghese <jobsonvarghese1@gmail.com>
2021-05-31 23:40:07 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,7 @@ sr.reveal('.contact__text', {interval: 200})
 sr.reveal('.contact__input', {delay: 400})
 sr.reveal('.contact__button', {delay: 600})
 
+sr.reveal('.fade', {delay: 500})
 
 
 // define all UI variable
